1be3e6ad0dbe8cec1c725ca6572b19c671e5c45a
dreambo8563 <dreambo8563@outlook.com>
2019-03-21 14:20:06 +0800

    test(more tc for simple):

--- tests/unit/main.spec.ts
+++ tests/unit/main.spec.ts
@@ -5,55 +5,10 @@ describe("base class", () => {
     const base = LazyBase.lazy(1);
     expect(base.value()).toBe(1);
   });
-  it("add", () => {
-    const base = LazyBase.lazy(1).add(4);
-    expect(base.value()).toBe(5);
-  });
-
-  it("subtract", () => {
-    const base = LazyBase.lazy(1).subtract(4);
-    expect(base.value()).toBe(-3);
-  });
-
-  it("multiply", () => {
-    const base = LazyBase.lazy(1).multiply(4);
-    expect(base.value()).toBe(4);
-  });
-  it("multiply - float", () => {
-    const base = LazyBase.lazy(1.5).multiply(4);
-    expect(base.value()).toBe(6);
-  });
-
-  it("divide - round", () => {
-    const base = LazyBase.lazy(1.5)
-      .divide(2)
-      .round(2);
-    expect(base.value()).toBe(0.75);
-  });
-
-  it("divide - floor", () => {
-    const base = LazyBase.lazy(1.5)
-      .divide(2)
-      .floor(1);
-    expect(base.value()).toBe(0.7);
-  });
-  it("divide - ceil", () => {
-    const base = LazyBase.lazy(1.5)
-      .divide(2)
-      .ceil(1);
-    expect(base.value()).toBe(0.8);
-  });
-  it("default", () => {
-    const base = LazyBase.lazy(1.5)
-      .divide(0)
-      .default(99);
-    expect(base.value()).toBe(99);
-  });
-
-  it("do", () => {
-    const base = LazyBase.lazy({ num: 1.5 })
-      .do((x: { num: number }) => x.num)
-      .multiply(2);
-    expect(base.value()).toBe(3);
+  it("stream with init value", () => {
+    const base1 = LazyBase.lazy(1);
+    const base2 = LazyBase.lazy(4);
+    const stream1 = LazyBase.stream(base1).add(base2);
+    expect(stream1.value()).toBe(5);
   });
 });
